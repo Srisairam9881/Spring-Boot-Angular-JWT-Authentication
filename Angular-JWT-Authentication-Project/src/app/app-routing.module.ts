597e65eef17e17d8c8ec40a388ad72b12df3ab0e
ALLAM SRI SAI RAM <80576654+Srisairam9881@users.noreply.github.com>
2022-05-26 13:23:23 +0530

    Add files via upload

---
+++ Angular-JWT-Authentication-Project/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BoardAdminComponent } from './Components/board-admin/board-admin.component';
+import { BoardModeratorComponent } from './Components/board-moderator/board-moderator.component';
+import { BoardUserComponent } from './Components/board-user/board-user.component';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { RegisterComponent } from './Components/register/register.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'user', component: BoardUserComponent },
+  { path: 'mod', component: BoardModeratorComponent },
+  { path: 'admin', component: BoardAdminComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
